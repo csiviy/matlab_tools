e6aeb230aebcf79f5de94661f161c866cd783b2e
Chris Siviy <csiviy@g.harvard.edu>
2018-06-01 11:06:00 -0400

    Added function to trim axes

---
+++ plotting/trim_axes.m
@@ -0,0 +1,68 @@
+function trim_axes(varargin)
+% TRIM_AXES Trim axes, similar to seaborn.despine(trim=True)
+% 
+% 
+%     USAGE:
+%         trim_axes(varargin)
+%
+%
+%     INPUTS:
+%         varargin: 
+%
+%
+%     OUTPUTS:
+%
+%
+%     SEE ALSO:
+%
+%
+% Chris Siviy, 01-Jun-2018 10:28 AM
+
+% Parse inputs
+p = inputParser();
+p.addParameter('ax', gca, @(x) isa(x, 'matlab.graphics.axis.Axes'))
+p.addParameter('grow', false)
+
+p.parse(varargin{:})
+
+ax = p.Results.ax;
+grow = p.Results.grow;
+
+if grow
+    % Grow the axes a bit
+    ax.XLim(1) = ax.XLim(1) - (ax.XTick(2) - ax.XTick(1)) / 4;
+    ax.YLim(1) = ax.YLim(1) - (ax.YTick(2) - ax.YTick(1)) / 4;
+    ax.XLim(2) = ax.XLim(end) + (ax.XTick(2) - ax.XTick(1)) / 4;
+    ax.YLim(2) = ax.YLim(end) + (ax.YTick(2) - ax.YTick(1)) / 4;
+end
+
+% Add a listener to update the axes limits
+addlistener(ax, 'MarkedClean', @(obj, event) resetVertex(obj));
+
+end
+
+function resetVertex(ax)
+% Reset the vertex, to make the changes persisent
+
+% For the bottom left corner
+ax.XRuler.Axle.VertexData(1,1) = ax.XTick(1);
+ax.YRuler.Axle.VertexData(2,1) = ax.YTick(1);
+
+% The upper edges of the x- and y-axes
+ax.YRuler.Axle.VertexData(2,2) = ax.YTick(end);
+ax.XRuler.Axle.VertexData(1,2) = ax.XTick(end);
+
+% If the box is on, do the rest of the axes as well
+if strcmp(ax.Box, 'on')
+    % For the upper left corner
+    ax.XRuler.Axle.VertexData(1,3) = ax.XTick(1);
+    
+    % For the bottom right corner
+    ax.YRuler.Axle.VertexData(2,3) = ax.YTick(1);
+
+    % For the upper right corner
+    ax.XRuler.Axle.VertexData(1,end) = ax.XTick(end);
+    ax.YRuler.Axle.VertexData(2,4) = ax.YTick(end);
+end
+
+end
